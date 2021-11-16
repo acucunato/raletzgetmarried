64c0b91a34cbf98ee65e70c1182d52ec84f7c4dd
Alyssa Cucunato <acucunato18@gmail.com>
2021-11-16 09:08:15 -0500

    updated photo gallery

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -4,116 +4,55 @@ import "./Photos.css";
 import surprised from "../../img/IMG_6785.jpg";
 import ringhand from "../../img/IMG_6741.jpg";
 import photo1 from "../../img/morganswedding.jpg";
-import photo2 from "../../img/seaisle.jpg";
-import nye from "../../img/2019.jpg";
 import sandiego from "../../img/sandiego.jpg";
 import alexaswedding from "../../img/alexawedding.jpg";
-import lola from "../../img/IMG_6915.jpeg";
+import lola from "../../img/IMG_6915.jpg";
 import lolamemike from "../../img/lolamikeme.jpg";
-import sunset from "../../img/sunset.jpg";
+import boat from "../../img/boat.jpg";
+import seaisle from "../../img/seaisle.jpg";
+import shore from "../../img/shore.jpg";
 
 function Photos() {
   return (
     <>
-      <div className="photo-gallery">
+      <div className="photo-gallery container">
         <div className="row">
           <Nav />
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <h1>our photo gallery</h1>
-            <img
-              src={surprised}
-              className="size350"
-              width="350"
-              height="550"
-              alt="monogram"
-            />
+          <div className="col-md-3 p-3 text-center wow fadeIn delay-2s">
+            <img src={surprised} className="size350" alt="gallery" />
           </div>
-          <div className="col-md-4 pt-5 text-center wow fadeIn delay-2s">
-            <img
-              src={ringhand}
-              className="size350"
-              width="350"
-              height="550"
-              alt="monogram"
-            />
+          <div className="col-md-3 p-3 text-center wow fadeIn delay-2s">
+            <img src={ringhand} className="size350" alt="gallery" />
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={photo1}
-              className="snapchat"
-              width="310"
-              height="550"
-              alt="monogram"
-            />
+          <div className="col-md-3 p-3 text-center wow fadeIn delay-2s">
+            <img src={photo1} className="snapchat" alt="gallery" />
           </div>
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={photo2}
-              className="snapchat"
-              width="310"
-              height="550"
-              alt="monogram"
-            />
+          <div className="col-md-3 p-3 text-center wow fadeIn delay-2s">
+            <img src={sandiego} className="snapchat" alt="gallery" />
           </div>
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={sandiego}
-              className="snapchat"
-              width="310"
-              height="550"
-              alt="monogram"
-            />
+          <div className="col-md-3 p-3 text-center wow fadeIn delay-2s">
+            <img src={seaisle} className="size350" alt="gallery" />
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-md-6 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={alexaswedding}
-              className="alexaswedding"
-              width="600"
-              height="400"
-              alt="monogram"
-            />
+          <div className="col-md-6 p-3 text-center wow fadeIn delay-2s">
+            <img src={alexaswedding} className="alexaswedding" alt="gallery" />
           </div>
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={nye}
-              className="size310"
-              width="310"
-              height="400"
-              alt="monogram"
-            />
+          <div className="col-md-3 p-3 text-center wow fadeIn delay-2s">
+            <img src={boat} className="size350" alt="gallery" />
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={lola}
-              className="size310"
-              width="310"
-              height="450"
-              alt="monogram"
-            />
+          <div className="col-md-2 p-3 text-center wow fadeIn delay-2s">
+            <img src={lola} className="size310" alt="gallery" />
           </div>
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={sunset}
-              className="size310"
-              width="310"
-              height="450"
-              alt="monogram"
-            />
+          <div className="col-md-2 p-3 text-center wow fadeIn delay-2s">
+            <img src={lolamemike} className="memikelola" alt="gallery" />
           </div>
-          <div className="col-md-3 pt-3 text-center wow fadeIn delay-2s">
-            <img
-              src={lolamemike}
-              className="memikelola"
-              width="350"
-              height="450"
-              alt="monogram"
-            />
+          <div className="col-md-5 p-3 text-center wow fadeIn delay-2s">
+            <img src={shore} className="memikelola" alt="gallery" />
           </div>
         </div>
       </div>

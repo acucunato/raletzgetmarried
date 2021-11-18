aaf9668aa2ce57a3d77546cc87047545f5cd062e
Alyssa Cucunato <acucunato18@gmail.com>
2021-11-18 13:15:23 -0500

    added bridesmaids

--- src/components/BridalParty/BridalParty.js
+++ src/components/BridalParty/BridalParty.js
@@ -1,21 +1,102 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 import "./BridalParty.css";
+import bridesmaids from "./bridesmaids.json";
+import groomsman from "./groomsmen.json";
+import kids from "./flowergirlringbearer.json";
 
 function BridalParty() {
   return (
     <>
-      <>
-        <div className="registry">
-          <div className="row">
-            <Nav />
-            <div className="col-md-4 pt-3 text-center wow fadeIn delay-2s">
-              <h1>coming soon</h1>
-              <div className="horizontal-line-sm"></div>
+      <div className="wedding-party">
+        <div className="row">
+          <Nav />
+          <div className="col-md-3 pt-3 p-1 text-center wow fadeIn delay-2s">
+            <div className="card">
+              <img
+                className="card-img-top rounded-circle weddingparty-img mx-auto"
+                src="https://i.ibb.co/7X1wyFq/gina.jpg"
+                alt="wedding-party"
+              />
+              <div className="card-body">
+                <h1 className="card-title">gina cucunato</h1>
+                <b className="card-text">maid of honor</b>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 pt-3 p-1 text-center wow fadeIn delay-2s">
+            <div className="card">
+              <img
+                className="card-img-top rounded-circle weddingparty-img mx-auto"
+                src="https://i.ibb.co/YpWGzv6/valen.jpg"
+                alt="wedding-party"
+              />
+              <div className="card-body">
+                <h1 className="card-title">valen fecca</h1>
+                <b className="card-text">matron of honor</b>
+              </div>
             </div>
           </div>
         </div>
-      </>
+        {/* bridesmaids */}
+        <div className="row justify-content-center weddingparty-rows">
+          {bridesmaids.map((bridesmaid) => (
+            <div className="col-md-4 pt-3 p-1 text-center wow fadeIn delay-2s">
+              <div className="card">
+                <img
+                  className="card-img-top rounded-circle weddingparty-img mx-auto"
+                  src={bridesmaid.photo}
+                  alt="wedding-party"
+                />
+                <div className="card-body">
+                  <h1 className="card-title">{bridesmaid.name}</h1>
+                  <b className="card-text">{bridesmaid.title}</b>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="horizontal-line-sm wow fadeIn delay-2s"></div>
+        {/* groomsmen */}
+        <div className="row justify-content-center weddingparty-rows">
+          {groomsman.map((groomsman) => (
+            <div className="col-md-4 pt-3 p-1 text-center wow fadeIn delay-2s">
+              <div className="card">
+                <img
+                  className="card-img-top rounded-circle weddingparty-img mx-auto"
+                  src={groomsman.photo}
+                  alt="wedding-party"
+                />
+                <div className="card-body">
+                  <h1 className="card-title">{groomsman.name}</h1>
+                  <b className="card-text">{groomsman.title}</b>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="horizontal-line-sm wow fadeIn delay-2s"></div>
+
+        {/* flower girl ring bearer */}
+        <div className="row justify-content-center weddingparty-rows">
+          {kids.map((kid) => (
+            <div className="col-md-4 pt-3 p-1 text-center wow fadeIn delay-2s">
+              <div className="card">
+                <img
+                  className="card-img-top rounded-circle weddingparty-img mx-auto"
+                  src={kid.photo}
+                  alt="wedding-party"
+                />
+                <div className="card-body">
+                  <h1 className="card-title">{kid.name}</h1>
+                  <b className="card-text">{kid.title}</b>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }

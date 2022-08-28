1eede8b09836e6acb2fd7403647d5650bec90856
Alyssa Cucunato <acucunato18@gmail.com>
2022-08-28 13:16:28 -0400

    updates

--- src/components/RSVP/RSVP.js
+++ src/components/RSVP/RSVP.js
@@ -7,11 +7,10 @@ function RSVP() {
       <div className="row">
         <Nav />
         <div className="col-md-3 pt-3 p-1 text-center wow fadeIn">
-          <h1 className="rsvp-title justify-content-center">rsvp</h1>
-          <div className="pb-3 justify-content-center">
-            <h5 className="pt-5">
-              <b>click below to rsvp</b>
-            </h5>
+          <h1 className="rsvp-title justify-content-center">
+            we hope you can celebrate with us!
+          </h1>
+          <div className="pb-3 pt-5 justify-content-center">
             <div>
               <a
                 href="https://www.theknot.com/us/alyssa-cucunato-and-michael-raletz-nov-2022/rsvp"
@@ -19,7 +18,7 @@ function RSVP() {
                 rel="noreferrer"
               >
                 <button type="button" class="btn btn-black">
-                  rsvp
+                  click to rsvp
                 </button>
               </a>
             </div>
